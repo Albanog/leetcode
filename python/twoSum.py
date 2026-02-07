bb6296f9f3d2fb3c4626c0a05399fa1257c6bc55
Albano Galiana <albano_g95@hotmail.com>
2026-02-07 19:54:07 -0300

    2sum act

--- python/twoSum.py
+++ python/twoSum.py
@@ -13,14 +13,6 @@ class Solution:
                 explorados[nums[i]]=i
             
             i += 1
-        # while i < len(nums):
-        #     while j < len(nums):
-        #         if nums[i] + nums[j] == target:
-        #             result[0] = i
-        #             result[1] = j
-        #             return result
-        #         j += 1
-        #     i += 1
-        #     j = i + 1
+        
 
         return result
