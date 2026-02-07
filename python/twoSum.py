97ef99843ad494ca358d2d660a588a3949fa931f
Albano Galiana <albano_g95@hotmail.com>
2026-02-07 19:53:45 -0300

    2sum act

--- python/twoSum.py
+++ python/twoSum.py
@@ -4,14 +4,23 @@ class Solution:
         j = 1
         result = [0, 0]
 
+        explorados = {}
         while i < len(nums):
-            while j < len(nums):
-                if nums[i] + nums[j] == target:
-                    result[0] = i
-                    result[1] = j
-                    return result
-                j += 1
+            if explorados.get(target - nums[i]) != None:
+                return [i, explorados.get(target - nums[i])]
+
+            if explorados.get(nums[i]) == None:
+                explorados[nums[i]]=i
+            
             i += 1
-            j = i + 1
+        # while i < len(nums):
+        #     while j < len(nums):
+        #         if nums[i] + nums[j] == target:
+        #             result[0] = i
+        #             result[1] = j
+        #             return result
+        #         j += 1
+        #     i += 1
+        #     j = i + 1
 
         return result
